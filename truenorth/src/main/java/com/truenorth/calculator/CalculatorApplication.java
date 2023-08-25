db4c7f2e44172adf9a911625a0e083ea0a33a892
Frangel Rodriguez <frangele331@gmail.com>
2023-08-24 20:32:09 -0400

    Add data mock for entities in running implementing CommandLineRunner

---
+++ truenorth/src/main/java/com/truenorth/calculator/CalculatorApplication.java
@@ -0,0 +1,84 @@
+package com.truenorth.calculator;
+
+import com.truenorth.calculator.model.OperationType;
+import com.truenorth.calculator.model.User;
+import com.truenorth.calculator.model.UserStatus;
+import com.truenorth.calculator.repository.OperationTypeRepository;
+import com.truenorth.calculator.repository.UserRepository;
+import com.truenorth.calculator.repository.UserStatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+@EnableSwagger2
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserStatusRepository userStatusRepository;
+
+    @Autowired
+    private OperationTypeRepository operationTypeRepository;
+
+
+    @Bean
+    CommandLineRunner init() {
+
+
+        List<OperationType> operationTypeList = new ArrayList<>();
+
+
+       // operationTypeRepository.saveAll(operationTypeList);
+
+
+        List<UserStatus> userStatusList = new ArrayList<>();
+
+
+
+        return args -> {
+
+            //User status
+            userStatusList.add(new UserStatus(1,"ACTIVE"));
+            userStatusList.add(new UserStatus(2,"INACTIVE"));
+
+
+            //operation Types
+            operationTypeList.add(new OperationType(1,"ADDITION",5));
+            operationTypeList.add(new OperationType(2,"SUBSTRACTION",6));
+            operationTypeList.add(new OperationType(3,"MULTIPLICATION",18));
+            operationTypeList.add(new OperationType(4,"DIVISION",20));
+            operationTypeList.add(new OperationType(5,"SQUARE_ROOT",15));
+            operationTypeList.add(new OperationType(6,"RANDOM_STRING",10));
+
+
+
+            userStatusRepository.saveAll(userStatusList);
+
+            Stream.of("truenort", "frangel","trueNorth2").forEach(name -> {
+                User user = new User(1L ,name.toLowerCase() + "@domain.com","password123",20.0 ,UserStatus.ACTIVE);
+                userRepository.save(user);
+            });
+
+
+            operationTypeRepository.saveAll(operationTypeList);
+
+           // userRepository.findAll().forEach(System.out::println);
+        };
+    }
+
+}
